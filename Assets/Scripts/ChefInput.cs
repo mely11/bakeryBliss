97d6090d3fa336872061d4dc0cfdaca254ee6693
11! <yiyiyuan@usc.edu>
2023-09-18 14:00:50 -0700

    #1,2,3 resolved ※
    🟥 √  Create a basic design of the level 1 scene (horizontal movement + jump + climb)
    🟥 √  Move the player with basic mechanics (not bug-free)
    🟥 √  Design the platforms in a level 1 scene (with the ingredients that the player will pick up)
    🟥 ~20 hrs

---
+++ Assets/Scripts/ChefInput.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Chef))]
+public class ChefInput : MonoBehaviour
+{
+    private Chef chef;
+
+    private void Start() => chef = GetComponent<Chef>();
+
+    private void Update()
+    {
+        // Read directional input from the player.
+        Vector2 inputDir = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        chef.SetDirectionalInput(inputDir);
+
+        // Handle jump input.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            chef.OnJumpDown();
+        }
+
+        // Handle releasing the jump button.
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            chef.OnJumpUp();
+        }
+    }
+}
