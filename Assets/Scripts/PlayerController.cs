a19a64181bf6449e652ab15b7e8e87c346b039aa
Sarah Kurdoghlian <sarahkurdoghlian@Sarahs-MacBook-Pro-2.local>
2023-09-17 20:37:37 -0700

    updating player jumping controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,8 +10,12 @@ public class PlayerController : MonoBehaviour
     
     // properties
     public float speed = 5.0f;
-    public float jumpPower = 8.0f;
+    public float maxSpeedMultiplier = 1.0f;
+    public float jumpPower = 10.0f;
     public bool isGrounded = true;
+    public bool isAscending;
+    public bool isDescending;
+    private float maxFallVelocity = 40.0f;
     
     // Components
     private Vector2 gravity;
@@ -27,11 +31,26 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        setAscendingDescending();
+        
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         
         // -------------- horizontal mechanics --------------
-        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        //transform.Translate(Vector2.right * Time.deltaTime * horizontalInput * speed);
+        if (Math.Abs(horizontalInput) == 1.0f)
+        {
+            if (maxSpeedMultiplier < 2.0f)
+            {
+                maxSpeedMultiplier += 0.1f;
+            }
+        }
+        else
+        {
+            maxSpeedMultiplier = 1.0f;
+        }
+        rb.velocity = new Vector2(horizontalInput * speed * maxSpeedMultiplier, rb.velocity.y);
+        
 
         // -------------- jump mechanics -------------
         if (isGrounded)
@@ -41,16 +60,27 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpPower * jumpInput);
         }
 
+        // slow down the jump velocity once they release the jump button
+        if (Input.GetButtonUp("Jump") && !isGrounded)
+        {
+            if (rb.velocity.y > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            }
+        }
+
         if (rb.velocity.y < 0)
         {
             // if player has started falling down, then increase its speed
-            rb.velocity -= gravity * Time.deltaTime * Math.Abs(transform.position.y - currentGround.transform.position.y);
+            rb.velocity -= gravity 
+                           * Time.deltaTime 
+                           * Math.Max(Math.Abs(transform.position.y - currentGround.transform.position.y), -maxFallVelocity);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.name.Contains("Floor"))
+        if (other.gameObject.name.Contains("Floor") && (!isAscending || !isDescending))
         {
             isGrounded = true;
             currentGround = other.gameObject;
@@ -62,6 +92,25 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.name.Contains("Floor"))
         {
             isGrounded = false;
+        }
+    }
+
+    private void setAscendingDescending()
+    {
+        if (rb.velocity.y > 0)
+        {
+            isAscending = true;
+            isDescending = false;
+        }
+        else if (rb.velocity.y < 0)
+        {
+            isDescending = true;
+            isAscending = false;
+        }
+        else
+        {
+            isDescending = false;
+            isAscending = false;
         }
     }
 }
